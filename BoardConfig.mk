293ebcb598e0804d94b95b292c67f9ccd02a7eee
Arpon7691 <arponsarkar226@gmail.com>
2019-03-15 10:33:21 +0000

    rolex:update vendor security patch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -226,7 +226,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
 BOARD_ROOT_EXTRA_FOLDERS := persist
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-01-05
+VENDOR_SECURITY_PATCH := 2019-02-05
 
 # SELinux
 #include device/qcom/sepolicy/sepolicy.mk
